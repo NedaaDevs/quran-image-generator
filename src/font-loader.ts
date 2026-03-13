1875f38fd404cf152ccccbff001e33411db1048c
Fahad <42780409+F2had@users.noreply.github.com>
2026-03-13 11:24:40 +0300

    fix: type version parameter as FontVersion

--- src/font-loader.ts
+++ src/font-loader.ts
@@ -1,8 +1,9 @@
 import { GlobalFonts } from "@napi-rs/canvas";
 import { existsSync } from "fs";
 import path from "path";
+import type { FontVersion } from "./types";
 
-export const registerPageFont = (fontsDir: string, page: number, version = "v2") => {
+export const registerPageFont = (fontsDir: string, page: number, version: FontVersion) => {
   const fontFamily = `${version}_p${page}`;
   const fontPath = path.join(fontsDir, `p${page}.ttf`);
 
